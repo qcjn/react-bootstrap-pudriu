bc61542bca1c1b7ce2acf6cd879af2907babaa70
qcjn <qcjn.gh@shungeng.top>
2020-07-10 07:20:23 +0800

    1 file modified

--- Action.js
+++ Action.js
@@ -4,5 +4,10 @@ import Layout from "./Layout";
 export default () => (
   <Layout>
     <h1>Action</h1>
+    <ul>{listItems}</ul>
   </Layout>
 )
+
+
+const arr = [1, 2 , 3]
+const listItems = arr.map(num => <li>{num}</li>)
