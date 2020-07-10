bdd025d09fb9c2c60c99a0692a833e8e8c9440a0
qcjn <qcjn.gh@shungeng.top>
2020-07-10 17:08:29 +0800

    1 file modified

--- Layout.js
+++ Layout.js
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 
 export default ({ children }) => (
   <div className="container">
-      <br />
-      {children}
+    <br />
+    {children}
   </div>
 );
