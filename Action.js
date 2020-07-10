3008d0b6cd67836baccccea9d6cd7cae816331d2
qcjn <qcjn.gh@shungeng.top>
2020-07-10 16:16:59 +0800

    1 file modified

--- Action.js
+++ Action.js
@@ -4,15 +4,34 @@ import Layout from "./Layout";
 export default () => (
   <Layout>
     <h1>Action</h1>
-    <ul>{listItems}</ul>
+
+    <table className="table">
+      <thead className="thead-light">
+        <tr>
+          <th scope="col">#</th>
+          <th scope="col">First</th>
+          <th scope="col">Last</th>
+          <th scope="col">Handle</th>
+        </tr>
+      </thead>
+      <tbody>
+        {listItems}
+      </tbody>
+    </table>
   </Layout>
 )
 
 
-const arr = [{name:"a"}, {name:"b"} , {name:"c"}]
+const arr = [
+  {first:"a", last:"aa", handle:"aaa"},
+  {first:"b", last:"bb", handle:"bbb"}, 
+  {first:"c", last:"cc", handle:"ccc"}
+]
 const listItems = arr.map(item => (
-  <div>
-  name: {item.name}
-  </div>
+  <tr>
+    <th scope="row">{arr.indexOf(item) + 1}</th>
+    <td>{item.first}</td>
+    <td>{item.last}</td>
+    <td>{item.handle}</td>
+  </tr>
 ))
- 
