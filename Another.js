2ae8ac4aa5fa558a812a87df8436aa7899609f0e
qcjn <qcjn.gh@shungeng.top>
2020-07-11 14:28:33 +0800

    1 file modified

--- Another.js
+++ Another.js
@@ -3,6 +3,6 @@ import Layout from "./Layout";
 
 export default () => (
   <Layout>
-    <h1>Another action</h1>{REACT_APP_NOT_SECRET_CODE}
+    <h1>Another action</h1>{process.env.REACT_APP_NOT_SECRET_CODE}
   </Layout>
 )
